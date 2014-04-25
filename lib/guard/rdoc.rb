1879be1f3d9c5cf1d2cad2cc4dd1ec0ae48c1c07
Bradley J. Spaulding <bspaulding@dataxu.com>
2014-04-25 13:00:36 -0700

    Guard::Rdoc passes options to Rdoc

--- lib/guard/rdoc.rb
+++ lib/guard/rdoc.rb
@@ -1,7 +1,21 @@
 require "guard/rdoc/version"
+require "guard"
+require "guard/plugin"
 
 module Guard
-  module Rdoc
-    # Your code goes here...
+  class Rdoc < Plugin
+    def run_on_changes(paths)
+      rdoc.document(rdoc_options + paths)
+    end
+
+    def rdoc_options
+      options.map {|k,v| "--#{k}=#{v}" }
+    end
+
+    private
+
+    def rdoc
+      @rdoc ||= RDoc::RDoc.new
+    end
   end
 end
